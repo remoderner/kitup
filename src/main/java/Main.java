6175c1fb7e865ec17b933436007202118336965d
ReModerner <remoderner@gmail.com>
2017-11-12 01:48:57 +0700

    del logger

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,7 @@
 import bin.GUIGenerator;
 
-import java.io.IOException;
-import java.util.logging.LogManager;
-
 public class Main {
     public static void main(String[] args) {
-        try {
-            LogManager.getLogManager().readConfiguration(
-                    Main.class.getResourceAsStream("logging.properties"));
-        } catch (IOException e) {
-            System.err.println("Could not setup logger configuration: " + e.toString());
-        }
-
         GUIGenerator guiGenerator = new GUIGenerator();
         guiGenerator.launchGUI(args);
     }
