497d68898a11ada4c873ea8483e24868f8201ec3
remoderner <32636111+remoderner@users.noreply.github.com>
2017-11-12 15:24:39 +0700

    add logging log4j2 (#5)

--- src/main/java/utils/FileCopyer.java
+++ src/main/java/utils/FileCopyer.java
@@ -1,5 +1,8 @@
 package utils;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,6 +11,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 class FileCopyer {
+    private static final Logger log = LogManager.getLogger(FileCopyer.class);
     FileCopyer() {
     }
 
@@ -16,11 +20,11 @@ class FileCopyer {
         File folder = new File(lastversionDirName);
         File[] listOfFiles = folder.listFiles(File::isFile);
         Path destDir = Paths.get(componentDirName);
-        System.out.println("Copying files....");
+        log.info("Copying files....");
 
         if (listOfFiles != null) {
             for (File file : listOfFiles) {
-                System.out.println(file.getName());
+                log.info(file.getName());
                 if (!file.getName().endsWith(".ini")) { //Только файлы, т.е. не копируем папки
                     try {
                         Files.copy(file.toPath(), destDir.resolve(file.getName()), StandardCopyOption.REPLACE_EXISTING);
